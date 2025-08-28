9638788b920f549679d056268077746a88b19ee8
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-28 17:31:11 +0530

    authentication-done

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 // import package
 import express from "express"
 import {config} from "dotenv"
-
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
 // import files
 import { connectDb } from "./src/db/db.js";
+import authRoute from "./src/routes/auth.routes.js"
 
-// middlewares
+
+
 const app = express();
 config();
 
+// middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
+// routes
+app.use("/api/auth", authRoute)
 
 const port = process.env.PORT || 5000
 
