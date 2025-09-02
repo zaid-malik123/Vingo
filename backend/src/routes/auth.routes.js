6ffc49814ee43dfe1e3a7e2ca00146b53fe1436e
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-02 13:00:06 +0530

    create-routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, resetPassword, sendOtp, signup, verifyOtp } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 
 router.post("/signup", signup)
 router.post("/login", login)
 router.get("/logout", logout)
+router.post("/send-otp", sendOtp)
+router.post("/verify-otp", verifyOtp)
+router.post("/reset-password", resetPassword)
 
 export default router;
