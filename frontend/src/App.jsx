67a1cd383dd462928551f66588ffd491545a8855
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-04 19:20:00 +0530

    create-home-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,37 @@
-import {Routes, Route} from "react-router-dom"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import ForgotPassword from "./pages/ForgotPassword"
-import useGetCurrentUser from "./hooks/useGetCurrentUser"
-import { useSelector } from "react-redux"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
 
-
-export const serverUrl = "http://localhost:3000"
+export const serverUrl = "http://localhost:3000";
 const App = () => {
-  useGetCurrentUser()
-  const {user} = useSelector((state)=> state.userSlice)
+  useGetCurrentUser();
+  const { user } = useSelector((state) => state.userSlice);
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to={"/"} />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to={"/"} />}
+        ></Route>
+        <Route
+          path="/forgot-password"
+          element={!user ? <ForgotPassword /> : <Navigate to={"/"} />}
+        ></Route>
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to={"/login"} />}
+        ></Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
