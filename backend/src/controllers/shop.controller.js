66e89302c47b46c8d6070d3a8fb4f8078f561f87
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-06 04:07:40 +0530

    createShop-controller

--- backend/src/controllers/shop.controller.js
+++ backend/src/controllers/shop.controller.js
@@ -2,3 +2,30 @@ import { uploadImage } from "../service/imageKit.service.js"
 import Shop from "../models/shop.model.js"
 import Item from "../models/items.model.js"
 
+export const createShop = async (req, res, next)=>{
+try {
+    const {name, city, state, address} = req.body;
+
+    let image;
+
+    if(req.file){
+     image = await uploadImage(req.file)
+    }
+
+   const shop = await Shop.create({
+    name,
+    city, 
+    state,
+    address,
+    image: image.url,
+    owner: req.userId
+   })
+   
+   await shop.populate("owner")
+
+   return res.status(200).json("shop created successfully")
+
+} catch (error) {
+   res.status(500).json({ message: error.message }); 
+}
+}
--- backend/src/routes/shop.routes.js
+++ backend/src/routes/shop.routes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import multer from "multer"
+import { createShop } from "../controllers/shop.controller.js";
+import { isAuth } from "../middleware/isAuth.js";
 const upload = multer({storage: multer.memoryStorage()});
 const router = express.Router();
 
-
+router.post("/create",isAuth,upload.single("image"),createShop)
 
 export default router;
