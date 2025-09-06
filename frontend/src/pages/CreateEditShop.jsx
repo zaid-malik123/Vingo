39daa401a6651d21d907f069a567d5b4bede9ccb
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-06 22:34:01 +0530

    created shop-successfully

--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -3,16 +3,47 @@ import { useNavigate } from "react-router-dom";
 import { FaUtensils } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+import axios from "axios";
+import { serverUrl } from "../App";
+import { setMyShopData } from "../redux/ownerSlice";
 
 const CreateEditShop = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const { shop } = useSelector((state) => state.ownerSlice);
   const { user, currentCity, currentState, currentAddress } = useSelector((state) => state.userSlice);
   const [name, setName] = useState(shop?.name || "")
   const [city, setCity] = useState(shop?.address || currentCity)
   const [state, setState] = useState(shop?.state || currentState)
   const [address, setAddress] = useState(shop?.address || currentAddress)
-  
+  const [frontendImage, setFrontendImage] = useState(shop?.image || null)
+  const [backendImage, setBackendImage] = useState(shop?.image || null)
+
+  const handleImage = (e)=>{
+  const file = e.target.files[0]
+  setBackendImage(file)
+  setFrontendImage(URL.createObjectURL(file)) 
+  }
+
+  const handleSubmit = async (e)=>{
+   e.preventDefault(); 
+  try {
+    const formData = new FormData()
+    formData.append("name", name)
+    formData.append("city", city)
+    formData.append("state", state)
+    formData.append("address", address)
+    if(backendImage){
+      formData.append("image", backendImage)
+    }
+    const res = await axios.post(`${serverUrl}/api/shop/create-edit`,formData,{withCredentials:true})
+    console.log(res.data)
+    dispatch(setMyShopData(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+  }
+
   return (
     <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
       <div
@@ -30,7 +61,7 @@ const CreateEditShop = () => {
             {shop ? "Edit Shop" : "Add Shop"}
           </div>
         </div>
-        <form className="space-y-5">
+        <form onSubmit={(e)=>handleSubmit(e)} className="space-y-5">
           {/* Name    */}
           <div>
             <label className="block:text-sm font-medium text-gray-700 mb-1">
@@ -54,7 +85,11 @@ const CreateEditShop = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mt-2"
               type="file"
               accept="image/*"
+              onChange={handleImage}
             />
+            {frontendImage && <div className="mt-4">
+              <img className="w-full h-48 object-cover rounded-lg border" src={frontendImage} alt="" />
+            </div>}
           </div>
 
           {/* City & State */}
@@ -64,7 +99,7 @@ const CreateEditShop = () => {
                 Shop City
               </label>
               <input
-                onChange={(e)=> setCity(e.target.name)}
+                onChange={(e)=> setCity(e.target.value)}
                 value={city}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mt-2"
                 type="text"
