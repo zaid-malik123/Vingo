6ae04dc2661d1364641af4276bed514635692985
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-10 21:38:58 +0530

    map categories array in User dashborad

---
+++ frontend/src/component/CategoryCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryCard = () => {
+  return (
+    <div >
+      Category Card
+    </div>
+  )
+}
+
+export default CategoryCard
--- frontend/src/component/UserDashboard.jsx
+++ frontend/src/component/UserDashboard.jsx
@@ -1,9 +1,18 @@
 import Nav from './Nav'
-
+import { categories } from '../category'
+import CategoryCard from './CategoryCard'
 const UserDashboard = () => {
   return (
-    <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center'>
+    <div className='w-full min-h-screen bg-[#fff9f6] flex flex-col items-center overflow-y-auto'>
       <Nav/>
+      <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]'>
+       <h1 className='text-gray-800 text-2xl sm:text-3xl'>Inspiration for your first order</h1>
+       <div className='w-full'>
+         {categories.map((c,i)=>(
+          <CategoryCard c={c} key={i}/>
+         ))}
+       </div>
+      </div>
     </div>
   )
 }
