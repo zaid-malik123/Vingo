566a6a5e3176a072b050476501e38a361a43e30f
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-22 22:22:19 +0530

    feat(delivery-boy): add basic UI with welcome card and location

--- frontend/src/component/DeliveryBoy.jsx
+++ frontend/src/component/DeliveryBoy.jsx
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux';
 import Nav from './Nav'
 
 const DeliveryBoy = () => {
+  const { user } = useSelector((state) => state.userSlice);
   return (
     <div className="w-full min-h-screen bg-[#fff9f6] flex flex-col items-center overflow-y-auto">
       <Nav/>
+      <div className='w-full max-w-[800px] flex flex-col gap-5 items-center'>
+         <div className='bg-white rounded-2xl shadow-md p-5 flex flex-col justify-start text-center items-center w-[90%] border border-orange-100 gap-2'>
+           <h1 className='text-xl font-bold text-[#ff4d2d]'>Welcome,  {user.fullName}</h1>
+           <p className='text-[#ff4d2d]'><span className='font-semibold'>Latitude :</span> {user.location.coordinates[1]} , <span className='font-semibold'>Longitude :</span> {user.location.coordinates[0]}</p>
+         </div>
+      </div>
     </div>
   )
 }
